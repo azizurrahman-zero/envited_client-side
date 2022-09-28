c3052e757ba919d4c3a41b86ae941b7e7e04073c
Azizur Rahman <azizurrahman.zero@gmail.com>
2022-09-29 02:11:23 +0600

    event page completed

--- src/Pages/Event.js
+++ src/Pages/Event.js
@@ -12,12 +12,32 @@ const Event = () => {
         <p className="text-lg text-[#828282] helvetica-font">
           Hosted by <span className="font-bold">Elysia</span>
         </p>
-        <div>
-          <div>
+        <div className="flex flex-col gap-8 mt-16">
+          <div className="flex gap-5">
             <div className="icon-shadow flex justify-center items-center text-[#8456EC] h-14 w-14 rounded-xl bg-white">
               <ImCalendar className="text-3xl" />
             </div>
-            <div></div>
+            <div>
+              <h6 className="font-bold text-lg text-[#240D57] helvetica-font">
+                18 August 6:00PM
+              </h6>
+              <h6 className="text-lg text-[#240D57] helvetica-font">
+                to <span className="font-bold">19 August 1:00PM</span> UTC +10
+              </h6>
+            </div>
+          </div>
+          <div className="flex gap-5">
+            <div className="icon-shadow flex justify-center items-center text-[#8456EC] h-14 w-14 rounded-xl bg-white">
+              <ImMap className="text-3xl" />
+            </div>
+            <div>
+              <h6 className="font-bold text-lg text-[#240D57] helvetica-font">
+                Street Name
+              </h6>
+              <h6 className="text-lg text-[#240D57] helvetica-font">
+                Suburb, State, Postcode
+              </h6>
+            </div>
           </div>
         </div>
       </div>
